da9a4d8fccafa130a4cdd1ae06c41552c1af7f07
BARATH KUMAR S <50653477+barath99@users.noreply.github.com>
2020-01-15 14:55:31 +0530

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,4 +1,6 @@
+## Do not just run the file.... without modifying the firebase url.
+
 from firebase import firebase
 firebase = firebase.FirebaseApplication('https://connexion-f4945.firebaseio.com/', None)
-result = firebase.put('/Users/Arvinth/',{'College':'NIT Trichy'},None)
+result = firebase.put('/Users/Barath/',{'College':'NIT Trichy'},None)
 print (result)
